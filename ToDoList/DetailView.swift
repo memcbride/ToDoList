2f54168c4d106ce5ecd534a39936035b2b48d21b
MacModeler <memcbride@mac.com>
2024-09-12 11:13:32 -0400

    Lesson 5.3 - Build the ToDo List UI completed.

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -2,7 +2,7 @@
 // Project: ToDoList
 // File: DetailView.swift
 // Created by Mark McBride on 2024.09.05
-// Last Updated:  2024.09.05
+// Last Updated:  2024.09.12
 // GitHub: https://github.com/memcbride
 // ------------------------------------------------------
 // Copyright © 2024 by MacModeler.  All rights reserved.
@@ -11,27 +11,62 @@
 import SwiftUI
 
 struct DetailView: View {
-    var passedValue: String
+    @State var toDo: String
+    @State private var reminderIsOn = false
+    @State private var isCompleted = false
+//    @State private var dueDate = Date.now + 60*60*24
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: .now)!
+    @State private var notes = ""
+    
     @Environment(\.dismiss) private var dismiss
     var body: some View {
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You Are a Swifty Legend!\nAnd you passed over the value \(passedValue) ")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            Spacer()
-            Button("Get Back!") {
-                dismiss()
-            }
-            .buttonStyle(.borderedProminent)
+        List {
+            TextField("Enter To Do here", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Set Reminder:", isOn: $reminderIsOn)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            
+            DatePicker("Date:", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .padding(.bottom)
+                .disabled(!reminderIsOn)
+            
+            Text("Notes:")
+                .padding(.top)
+            
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            
+            Toggle("Completed:", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+
         }
-        .padding()
+        .listStyle(.plain)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //TODO: Add Save Code Here
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView(passedValue: "Item 1")
+    NavigationStack {
+        DetailView(toDo: "")
+    }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -2,7 +2,7 @@
 // Project: ToDoList
 // File: ToDoListView.swift
 // Created by Mark McBride on 2024.09.05
-// Last Updated:  2024.09.05
+// Last Updated:  2024.09.12
 // GitHub: https://github.com/memcbride
 // ------------------------------------------------------
 // Copyright © 2024 by MacModeler.  All rights reserved.
@@ -15,20 +15,37 @@ struct ToDoListView: View {
                  "Change the World",
                  "Bring the Awesome",
                  "Take a Vacation"]
+    @State private var sheetIsPresented = false
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
             }
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
             .listStyle(.plain)
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
