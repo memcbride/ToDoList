ceb2d58dbe8838b25dead37a6f79dc23025e6065
MacModeler <memcbride@mac.com>
2024-09-21 12:14:51 -0400

    Complete Lesson 5.6 - Deleting and Updating Checkmarks in SwiftData

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -20,10 +20,28 @@ struct ToDoListView: View {
         NavigationStack {
             List {
                 ForEach(toDos) { toDo in
-                    NavigationLink {
-                        DetailView(toDo: toDo)
-                    } label: {
-                        Text(toDo.item)
+                    HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .toggle on ToDoListView did not work.")
+                                    return }
+                            }
+                        
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(toDo)
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .delete on ToDoListView did not work.")
+                                    return }
+                            }
+                        }
                     }
                     .font(.title2)
                 }
